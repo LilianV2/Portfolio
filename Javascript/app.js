852553b6ef52c0f38631c907418a6f26cac70288
lilia <lilianom59@outlook.fr>
2023-02-04 20:10:58 +0100

    Portfolio VANHOORNE Lilian

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,2 +1,14 @@
 AOS.init();
 
+let mail = document.querySelector('.mail')
+let tel = document.querySelector('.tel')
+
+mail.addEventListener('click', () => {
+    mail.innerHTML = "lilianom59@outlook.fr";
+    mail.style.fontSize = "2rem";
+})
+
+tel.addEventListener('click', () => {
+    tel.innerHTML = "07 61 60 25 21";
+    tel.style.fontSize = "2rem";
+})
